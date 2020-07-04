bd0abfa03f32226f75922b82263a10bbcaedcc26
lalitchilka <lalitchilka@gmail.com>
2020-07-04 19:31:42 +0530

    Initial commit from spring initilizr project

---
+++ src/test/java/com/lalit/Spring5RecipeApp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalit.Spring5RecipeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
