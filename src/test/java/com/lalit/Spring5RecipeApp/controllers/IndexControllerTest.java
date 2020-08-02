75cebf2b27998f3c0781799cd76662e41493b15f
lalitchilka <lalitchilka@gmail.com>
2020-08-02 14:31:23 +0530

    Integration test for Unit of measure. Closes #13

--- src/test/java/com/lalit/Spring5RecipeApp/controllers/IndexControllerTest.java
+++ src/test/java/com/lalit/Spring5RecipeApp/controllers/IndexControllerTest.java
@@ -1,10 +1,14 @@
 package com.lalit.Spring5RecipeApp.controllers;
 
+import com.lalit.Spring5RecipeApp.domain.Recipe;
 import com.lalit.Spring5RecipeApp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +16,9 @@ import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -30,7 +37,17 @@ class IndexControllerTest {
     }
 
     @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
     void getIndexPage() {
+        Recipe recipe = new Recipe();
         String viewName = indexController.getIndexPage(model);
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
