57408e7fb22fb4f7f3013a32cbdff78d96360c07
lalitchilka <lalitchilka@gmail.com>
2020-10-25 16:07:37 +0530

    feat: Implement convert UOM to UOMC
    Closes: #40

--- src/main/java/com/lalit/Spring5RecipeApp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
+++ src/main/java/com/lalit/Spring5RecipeApp/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -2,10 +2,23 @@ package com.lalit.Spring5RecipeApp.converters;
 
 import com.lalit.Spring5RecipeApp.commands.UnitOfMeasureCommand;
 import com.lalit.Spring5RecipeApp.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 
-public class UnitOfMeasureToUnitOfMeasureCommand {
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
 
+    @Synchronized
+    @Nullable
+    @Override
     public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if(unitOfMeasure != null){
+            final UnitOfMeasureCommand unitOfMeasureCommand = new UnitOfMeasureCommand();
+            unitOfMeasureCommand.setId(unitOfMeasure.getId());
+            unitOfMeasureCommand.setDescription((unitOfMeasure.getDescription()));
+            return unitOfMeasureCommand;
+        }
         return null;
     }
 }
